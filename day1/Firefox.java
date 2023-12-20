ddbe0ba6ec9595913ec8bbff895746f6528fc221
shastiramtr <112606338+shastiramtr@users.noreply.github.com>
2023-12-20 22:09:42 +0530

    Day1
    DAY 1

---
+++ day1/Firefox.java
@@ -0,0 +1,18 @@
+package week1.day1;
+
+public class Firefox {
+
+	public static void main(String[] args) {
+		float BrowserVersion = 100.2f;
+	    String BrowserName = "Firefox";
+	    boolean isvisable = true;
+	    int ReleaseYear = 1998;
+	    char BrowserLogo = 'f';
+	    
+	    System.out.println("Browser Version = "+ BrowserVersion);
+	    System.out.println("Browser Name = "+ BrowserName );
+	    System.out.println("Browser Visable = "+ isvisable);
+	    System.out.println("Browser Realease year = "+ ReleaseYear);
+	    System.out.println("Browser Logo = "+ BrowserLogo);
+	}
+}
